df2efe971b64aa5d68e306cf5ee8b5faaa5e4d43
jorge <jorge_eduardo2894@hotmail.com>
2021-07-17 17:08:20 -0500

    Added contrasts to get differentially expressed genes and volcano plots

--- 3_dea.R
+++ 3_dea.R
@@ -15,10 +15,13 @@ library(affycoretools)
 setwd("/home/svillicaña/INMEGEN_LB")
 
 # Directories
+# rds.dir <- "/home/svillicaña/INMEGEN_LB/resultados/RDS/expression/"
+# plot.dir <- "/home/svillicaña/INMEGEN_LB/resultados/Plots/expression/"
+# csv.dir <- "/home/svillicaña/INMEGEN_LB/resultados/CSV/expression/"
+
 rds.dir <- "/home/svillicaña/INMEGEN_LB/resultados/RDS/expression/"
-plot.dir <- "/home/svillicaña/INMEGEN_LB/resultados/Plots/expression/"
-html.dir <- "/home/svillicaña/INMEGEN_LB/resultados/HTML/expression/"
-csv.dir <- "/home/svillicaña/INMEGEN_LB/resultados/CSV/expression/"
+plot.dir <- "/home/jpeña/INMEGEN_LB/resultados/Plots/expression/"
+csv.dir <- "/home/jpeña/INMEGEN_LB/resultados/CSV/expression/"
 
 # Sample sheet
 celfiles_raw <- read.table('data/sindrome_metabolico_group_file.txt',
@@ -117,29 +120,149 @@ diff.contrast <- function(fit.mod, cont.ind, f.name, max.n, p.thresh, lfc.thresh
   # Return differentially expressed genes table
   return(diff.genes)
 }
-#paste0(html.dir, f.name, ".html")
-cd.1 <- "hipotalamo vs. pulmon"
-cd.2 <- "hipotalamo vs. stem_cells"
-cd.3 <- "hipotalamo vs. tejido_adiposo"
 
-# First contrast (hipotalamo vs. pulmon)
-diff.list1 <- diff.contrast(fit.mod = fitCB, cont.ind = 1, f.name = "hipotalamo-pulmon",
-                            max.n = n_transcripts, 
-                            html.title = paste0("Differetial expression: ", cd.1),
-                            p.thresh = p.thresh, lfc.thresh = lfc.thresh)
+# Get the differential expressed genes for each contrast
+diff.list <- c()  # Vector to store differentially expressed data frames
 
-# Second contrast (hipotalamo vs. stem cells)
+contr.names <- colnames(contrasts) %>% str_replace_all(" ", "")
+for (contr.idx in 1:length(contr.names)){
+  
+  diff.genes <- diff.contrast(fit.mod = fitCB, cont.ind = contr.idx, 
+                              f.name = contr.names[contr.idx], max.n = n_transcripts,
+                              p.thresh = p.thresh, lfc.thresh = lfc.thresh)
+  
+  diff.list <- c(diff.list, diff.genes)
+}
 
-# Third contrast (hipotalamo vs. tejido adiposo)
+#---------------------#
+#### Volcano plots ####
+#---------------------#
 
+contrast.volcano <- function(fit.mod, title, cont.ind, pch.st = 20, p.thresh, lfc.thresh) {
+  # Transform p-value threshold to -log(p-value)
+  logP.thresh <- -log10(p.thresh) # The smaller a p-value gets, the bigger the -log(p-value)
+  
+  # Matrices of coefficients and logP
+  coef.mat <- as.matrix(fit.mod$coef) # logFC
+  logP.mat <- -log10(fit.mod$p.value) %>% as.matrix() # -log(p-value)
+  
+  # Set the x and y-axis
+  xl <- min(coef.mat[, cont.ind]) - 0.5
+  xu <- max(coef.mat[, cont.ind]) + 0.5
+  yl <- 0
+  yu <- max(logP.mat[, cont.ind]) + 0.5
+  
+  # Volcano base plot
+  volcanoplot(fit.mod, col="blue", ylim=c(yl, yu), xlim=c(xl, xu), coef = cont.ind,
+              main = title, cex.lab = 1.3, ylab = "-log(p-value)", style = "p-value", pch = pch.st)
+  
+  # Section the plot
+  par(new=T) # So the next plotted thing overlaps
+  abline(v=-lfc.thresh, col="brown", ylab="", xlab="")
+  par(new=T)
+  abline(v=lfc.thresh, col="brown", ylab="", xlab="")
+  par(new=T)
+  abline(h=logP.thresh, col="black", ylab="", xlab="")
+  
+  # Obtain indices of interest
+  ind1 = abs(coef.mat[, cont.ind]) > lfc.thresh # Values above log fold change threshold
+  ind2 = logP.mat[, cont.ind] > logP.thresh # Values above p-value threshold
+  ind3 = (coef.mat[, cont.ind] > lfc.thresh & logP.mat[, cont.ind] > logP.thresh) # Upper right quadrant 
+  ind4 = (coef.mat[, cont.ind] < -lfc.thresh & logP.mat[, cont.ind] > logP.thresh) # Upper left quadrant
+  
+  # Values above log fold change threshold
+  x = coef.mat[ind1, cont.ind]
+  y = logP.mat[ind1, cont.ind]
+  par(new=T)
+  plot(x, y, col="magenta",ylim=c(yl,yu), xlim=c(xl,xu),main="", pch = 20, 
+       xlab="", ylab="",cex.lab=1.3, cex = 0.35)
+  # Values above B threshold
+  x = coef.mat[ind2, cont.ind]
+  y = logP.mat[ind2, cont.ind]
+  par(new=T)
+  plot(x, y, col="orange",  ylim=c(yl,yu), xlim=c(xl,xu), main="", pch = 20, 
+       xlab="", ylab="",cex.lab=1.3, cex = 0.35)
+  # Upper right quadrant
+  x = coef.mat[ind3, cont.ind]
+  y = logP.mat[ind3, cont.ind]
+  par(new=T)
+  plot(x, y, col="red",  ylim=c(yl,yu), xlim=c(xl,xu), main="", pch = 20, 
+       xlab="", ylab="",cex.lab=1.3, cex = 0.35)
+  # Upper left quadrant
+  x = coef.mat[ind4, cont.ind]
+  y = logP.mat[ind4, cont.ind]
+  par(new=T)
+  plot(x, y, col="darkgreen", ylim=c(yl,yu), xlim=c(xl,xu), main="", pch = 20, 
+       xlab="", ylab="",cex.lab=1.3, cex = 0.35)
+}
 
+# Remove probes with no gene symbol
+probe.labs <- rownames(fitCB)
+syms <- f.data[probe.labs, "Symbol"]
+indx <- !is.na(syms)
+fitCBS <- fitCB[indx, ]
 
+# Volcano plots for all contrasts
+cd.list <- c(cd.1, cd.2, cd.3, cd.4, cd.5, cd.6)
+for (contr.idx in 1:length(contr.names)){
+  
+  # Save the plot as PDF
+  pdf(paste0(plot.dir, "volcano_", contr.names[contr.idx], ".pdf"), width = 14, height = 8)
+  contrast.volcano(fit.mod = fitCBS, cont.ind = contr.idx,
+                   title = paste0("Differential expression: ", cd.list[contr.idx]),
+                   p.thresh = p.thresh, lfc.thresh = lfc.thresh)
+  dev.off()
+}
 
+#----------------#
+#### Heatmaps ####
+#----------------#
 
+contrast.heatmap <- function(..., diff.expr, title, f.name) {
+  
+  # Expand the first arguments
+  # substitute just create a language object, it need to be evaluated
+  arrays <- substitute(list(...)) %>% eval() # This will have strings to select the columns from expr.mat
+  array.regex <- paste0("(", paste0(arrays, collapse="|"), ")") # Turn it into a single regex string
+  
+  # Select corresponding columns
+  cont.ind <- colnames(expr.mat) %>% grep(array.regex, .)
+  
+  # Get expression values from the differentially expressed genes
+  data.clus <- expr.mat[rownames(diff.expr), cont.ind] # These will be the ones for the cluster
+  
+  # Obtain their gene symbols
+  gene.syms <- diff.expr$Symbol
+  
+  # Set outer margin (oma) and margin (mar) arguments for the plot
+  par(oma = c(3,1,3,4), mar = c(12,5,2,2)+0.1)
+  
+  # Heatmap plot 
+  ind.hmap <- heatmap.2(data.clus, col=greenred(75), scale = "row", key = TRUE, symkey = FALSE, 
+                        density.info = "none", trace = "none", cexRow = 0.55, cexCol = 0.7, 
+                        main = title, labRow = gene.syms, ColSideColors = palette[cont.ind])
+  
+  # Genes 
+  idx <- rev(ind.hmap$rowInd) # Reverse order of indices heatmap matrix
+  genes.hmap <- cbind(data.clus[idx, ], diff.expr[idx, -1], diff.expr[idx, "sym"])
+  
+  # Write results
+  write.csv(genes.hmap, file = paste0(csv.dir, f.name, ".csv"))
+}
 
-
-
-
+# Heatmaps for all contrasts
+for (contr.idx in 1:length(contr.names)){
+  
+  # Split contrast groups
+  groups <- str_split(contr.names[contr.idx], "-")
+  
+  # Save the plot as PDF
+  pdf(paste0(plot.dir, "heatmap_", contr.names[contr.idx], ".pdf"), width = 9, height = 16)
+  contrast.heatmap(groups[1], groups[2], diff.expr = diff.list[contr.idx], 
+                   title = paste0("Expression heatmap: ", cd.list[contr.idx]), 
+                   f.name = paste0("expression-heatmap_", contr.names[contr.idx]))
+  dev.off()
+}
 
 
 
